c91589b8a1d6c258274e8ac1577c338a5857505a
zenklot <zenklot11@gmail.com>
2020-06-17 10:20:54 +0700

    Mengganti DT cdn ke local

--- assets/js/viktor.js
+++ assets/js/viktor.js
@@ -152,3 +152,15 @@ function EmptyAddUser(){
   $('#inName').val('');
 };
 
+
+function cekAlpabet(inputtxt)
+  {
+   var letters = /^[A-Za-z]+$/;
+   return letters.test(inputtxt);
+  }
+  
+
+function cekEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
