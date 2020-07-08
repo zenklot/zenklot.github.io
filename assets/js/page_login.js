552e7c5f402e968894b2c2c668dc2cc9f7028d76
zenklot <zenklot11@gmail.com>
2020-07-08 22:30:55 +0700

    Menambahkan Download dan Error handle sedikit

--- assets/js/page_login.js
+++ assets/js/page_login.js
@@ -8,6 +8,10 @@ function login(){
     let $email = document.getElementById("email-login").value;
     let $password = document.getElementById("password-login").value;
 
+
+    if(cekEmail($email) && $password !== ""){
+
+    try{
     var xhr = new XMLHttpRequest();
     xhr.open("POST", 'https://spk-psi.herokuapp.com/api/v1/auth', true);
 
@@ -15,16 +19,34 @@ function login(){
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
     xhr.onreadystatechange = function() { // Call a function when the state changes.
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            // Request finished. Do processing here.
+        if (this.readyState === XMLHttpRequest.DONE) {
+            if(this.status === 200){
+                // Request finished. Do processing here.
             let data = JSON.parse(xhr.responseText);
             // console.log(data);
             window.localStorage.setItem('token',data.access_token);
             window.localStorage.setItem('admin-name',data.name);
             window.location.replace('/admin');
+            }else if(this.status === 401){
+                alert('Tidak Ada User Dengan Email Tersebut!');
+            }
+            
         }
     }
+
     xhr.send("email="+$email+"&password="+$password);
+    }catch(e){
+        console.log('Takde');
+    }
+
+}else{
+    alert('Email Atau Password Salah!');
+}
    
 }
 
+
+function cekEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
